33f9fbc26dfb46db6c6f552aae18c244f40390f6
Matheus Nogueira Silveira <smatheusblu@gmail.com>
2025-09-21 02:57:33 -0300

    Create emacs literate config

---
+++ early-init.el
@@ -0,0 +1,8 @@
+;; Example Elpaca early-init.el -*- lexical-binding: t; -*-
+(setq package-enable-at-startup nil)
+(setq gc-cons-threshold (* 256 1024 1024))
+(setq read-process-output-max (* 4 1024 1024))
+(setq comp-deferred-compilation t)
+(setq comp-async-jobs-number 8)
+(setq gcmh-idle-delay 5)
+(setq gcmh-high-cons-threshold (* 1024 1024 1024))
